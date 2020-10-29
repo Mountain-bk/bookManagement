99b05b4973fe81cf3b337e9f2729c784406003ea
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-10-29 15:09:00 +0900

    change django project name

--- djangotask/urls.py
+++ djangotask/urls.py
@@ -1,4 +1,4 @@
-"""libraryProject URL Configuration
+"""djangotask URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
