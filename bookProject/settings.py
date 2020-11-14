c22d45e3f74af6a9dadb0fd584b05e9adc62a6e7
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-14 15:47:34 +0900

    change database to postgresql

--- bookProject/settings.py
+++ bookProject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'bookProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'booktask', 
+        'USER': 'ryo', 
+        'PASSWORD': 'password', 
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
