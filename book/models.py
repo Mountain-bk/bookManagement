868edc3a3b0edd6a11b596f4c7895c13ee7c0bed
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-18 21:05:30 +0900

    add 'Author' to models

--- book/models.py
+++ book/models.py
@@ -8,11 +8,22 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
+class Author(models.Model):
+    name = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     title = models.CharField(max_length = 100)
     published_date = models.DateField(auto_now = False)
     categories = models.ManyToManyField(Category)
+    authors = models.ManyToManyField(Author)
 
     def __str__(self):
         return self.title
+    
+
 
