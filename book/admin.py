9b5b3b5db7218b15d7d3397e55cc0cd18021e0f0
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-18 21:06:20 +0900

    modify admin.py

--- book/admin.py
+++ book/admin.py
@@ -1,7 +1,33 @@
 from django.contrib import admin
-from .models import Category, Book
+from .models import Category, Book, Author
 
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'published_date', 'get_categories', 'get_authors')  # 一覧に出したい項目
+    list_display_links = ('id', 'title', 'published_date', 'get_categories', 'get_authors')  # 修正リンクでクリックできる項目
+    list_filter = ['categories', 'authors'] # カテゴリー、著者でフィルターをかける
+
+    def get_categories(self, obj):  # 所属カテゴリーを取得
+        return " / ".join([categories.name for categories in obj.categories.all()])
+
+    def get_authors(self, obj):  # 著者を取得
+        return " / ".join([author.name for author in obj.authors.all()])
+    
+admin.site.register(Book, BookAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    list_display_links = ('id', 'name',)
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    list_display_links = ('id', 'name',)
+
+admin.site.register(Author, AuthorAdmin)
+
+
