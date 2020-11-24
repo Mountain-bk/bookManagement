06acb89566b512c71c50e5bd23a51acf82b06750
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-24 19:19:17 +0900

    import 'redirect', 'AuthorForm' and add author edit function on views.py

--- book/views.py
+++ book/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Author
 from django.contrib import messages
+from .forms import AuthorForm
 
 # Create your views here.
 def home_page(request):
@@ -10,3 +11,16 @@ def home_page(request):
 def author_view(request):
     authors = Author.objects.all()
     return render(request, 'book/author.html', {'authors': authors})
+
+
+def author_edit_view(request, id):
+    author = Author.objects.get(id=id)
+    if request.method == 'POST':
+        form = AuthorForm(request.POST, instance=author)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Update submission succesfull')
+            return redirect('book:author')
+    else:
+        form = AuthorForm(instance=author)
+    return render(request, 'book/author_edit.html', dict(form=form, id=id))
