3e973ad92cfdb271f4da2b4cc77e550172bb01ce
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-24 19:14:09 +0900

    import 'Author Model', contrib.messages and add author page function on views.py

--- book/views.py
+++ book/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Author
+from django.contrib import messages
 
 # Create your views here.
 def home_page(request):
     return render(request, 'book/index.html')
+
+
+def author_view(request):
+    authors = Author.objects.all()
+    return render(request, 'book/author.html', {'authors': authors})
