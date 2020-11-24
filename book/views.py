f07e0535a8c0c0bb619ead9933f1cd7e306bce74
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-24 13:38:54 +0900

    import auth.login and auth.logout on views.py

--- book/views.py
+++ book/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+
 
 # Create your views here.
 def home_page(request):
