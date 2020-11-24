a8d04d6044a7d8e9591080e4036214450ea308f3
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-24 13:25:27 +0900

    add author delete path on urls.py

--- book/urls.py
+++ book/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('author/', views.author_view, name='author'),
     path('author-edit/<int:id>', views.author_edit_view, name='author edit'),
     path('author-register/', views.author_register_view, name='author register'),
+    path('author-delete/<int:id>', views.author_delete_view, name='author delete')
 ]
