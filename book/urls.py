263ef8755d81fe47c4a7a5fe5f7af047b04b6594
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-24 19:28:04 +0900

    set path to author delete page on urls.py

--- book/urls.py
+++ book/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home_page),
     path('author/', views.author_view, name='author'),
     path('author-edit/<int:id>', views.author_edit_view, name='author edit'),
-    path('author-register/', views.author_register_view, name='author register')
+    path('author-register/', views.author_register_view, name='author register'),
+    path('author-delete/<int:id>', views.author_delete_view, name='author delete')
 ]
