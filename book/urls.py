012a94325cce59230aad301789beb8a97f3a1161
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-24 13:20:18 +0900

    add book edit path on urls.py

--- book/urls.py
+++ book/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('mypage/', views.mypage_view, name='mypage'),
     path('book-shelf/', views.book_shelf_view, name='book shelf'),
     path('book-register/', views.book_register_view, name='book register'),
+    path('book-edit/<int:id>', views.book_edit_view, name='book edit'),
 ]
