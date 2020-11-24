ae6c3696adcf69bdbc653a2e7703c6dec20a3bba
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-24 13:18:06 +0900

    add mypage path on urls.py

--- book/urls.py
+++ book/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_action, name='logout'),
+    path('mypage/', views.mypage_view, name='mypage'),
 ]
