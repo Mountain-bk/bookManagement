5693191cde3e8c2df7ce81a06a04cb17c3ab5b39
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-24 18:48:56 +0900

    set path to category edit page

--- book/urls.py
+++ book/urls.py
@@ -5,4 +5,5 @@ app_name = 'book'
 urlpatterns = [
     path('', views.home_page),
     path('category/', views.category_view, name='category'),
+    path('category-edit/<int:id>', views.category_edit_view, name='category edit')
 ]
