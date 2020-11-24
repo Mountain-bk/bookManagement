64b6a9a4fd3ed9eb21b345b98de5fef50cabbe59
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-24 13:39:33 +0900

    import models on views.py

--- book/views.py
+++ book/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
+from .models import Book, Category, Author
 
 
 # Create your views here.
