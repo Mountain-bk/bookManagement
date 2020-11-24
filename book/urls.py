ac034e869d3f11871e7444aad24da29cfbcf4606
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-24 19:18:00 +0900

    set path to author edit page on urls.py

--- book/urls.py
+++ book/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'book'
 urlpatterns = [
     path('', views.home_page),
-    path('author/', views.author_view, name='author')
+    path('author/', views.author_view, name='author'),
+    path('author-edit/<int:id>', views.author_edit_view, name='author edit')
 ]
