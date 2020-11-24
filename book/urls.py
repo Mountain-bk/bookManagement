36742ff58923fa81cdb070fb90df066f3d17c128
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-24 19:23:28 +0900

    set path to author register page on urls.py

--- book/urls.py
+++ book/urls.py
@@ -5,5 +5,6 @@ app_name = 'book'
 urlpatterns = [
     path('', views.home_page),
     path('author/', views.author_view, name='author'),
-    path('author-edit/<int:id>', views.author_edit_view, name='author edit')
+    path('author-edit/<int:id>', views.author_edit_view, name='author edit'),
+    path('author-register/', views.author_register_view, name='author register')
 ]
