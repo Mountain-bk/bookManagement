547de5721e186e76eaaf94da49531f86180de648
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-24 18:49:47 +0900

    add CategoryForm and add category edit function

--- book/views.py
+++ book/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Category
+from .forms import CategoryForm
 
 # Create your views here.
 def home_page(request):
@@ -9,4 +10,18 @@ def home_page(request):
 def category_view(request):
     categories = Category.objects.all()
     return render(request, 'book/category.html', {'categories': categories})
+
+
+def category_edit_view(request, id):
+    category = Category.objects.get(id=id)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Update submission succesfull')
+            return redirect('book:category')
+    else:
+        form = CategoryForm(instance=category)
+    return render(request, 'book/category_edit.html', dict(form=form, id=id))
+
 
