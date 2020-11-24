f281822f62b2c276cf3761c741802a7fb559e1c1
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-24 13:17:28 +0900

    add logout path on urls.py

--- book/urls.py
+++ book/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home_page, name='home'),
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
+    path('logout/', views.logout_action, name='logout'),
 ]
