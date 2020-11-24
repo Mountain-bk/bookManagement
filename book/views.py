10d35e4cbb260c112772dab041af0c0d8a8acf7c
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-24 13:47:18 +0900

    add category edit function on views.py

--- book/views.py
+++ book/views.py
@@ -110,3 +110,16 @@ def book_delete_view(request, id):
 def category_view(request):
     categories = Category.objects.all()
     return render(request, 'book/category.html', {'categories': categories})
+
+
+def category_edit_view(request, id):
+    category = Category.objects.get(id=id)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Update submission succesfull')
+            return redirect('book:category')
+    else:
+        form = CategoryForm(instance=category)
+    return render(request, 'book/category_edit.html', dict(form=form, id=id))
