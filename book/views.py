a841ea2f6acdcf442f9681bed15b37a27008503c
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-24 13:49:30 +0900

    add author edit function on views.py

--- book/views.py
+++ book/views.py
@@ -149,3 +149,16 @@ def category_delete_view(request, id):
 def author_view(request):
     authors = Author.objects.all()
     return render(request, 'book/author.html', {'authors': authors})
+
+
+def author_edit_view(request, id):
+    author = Author.objects.get(id=id)
+    if request.method == 'POST':
+        form = AuthorForm(request.POST, instance=author)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Update submission succesfull')
+            return redirect('book:author')
+    else:
+        form = AuthorForm(instance=author)
+    return render(request, 'book/author_edit.html', dict(form=form, id=id))
