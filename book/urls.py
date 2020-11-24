a8f054358880d964f6db7d651f8b35250cfd42c6
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-24 18:52:51 +0900

    set path to category register page

--- book/urls.py
+++ book/urls.py
@@ -5,5 +5,6 @@ app_name = 'book'
 urlpatterns = [
     path('', views.home_page),
     path('category/', views.category_view, name='category'),
-    path('category-edit/<int:id>', views.category_edit_view, name='category edit')
+    path('category-edit/<int:id>', views.category_edit_view, name='category edit'),
+    path('category-register/', views.category_register_view, name='category register')
 ]
