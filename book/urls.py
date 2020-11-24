1dd8c4626eefc756e86edd38f83e0a491ee11770
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-24 18:57:06 +0900

    set path to category delete page

--- book/urls.py
+++ book/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home_page),
     path('category/', views.category_view, name='category'),
     path('category-edit/<int:id>', views.category_edit_view, name='category edit'),
-    path('category-register/', views.category_register_view, name='category register')
+    path('category-register/', views.category_register_view, name='category register'),
+    path('category-delete/<int:id>', views.category_delete_view, name='category delete')
 ]
