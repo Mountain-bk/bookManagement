cf44be906063140ba3a45d91b2a83925911901c5
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-25 12:27:30 +0900

    set name for home page path on urls.py

--- book/urls.py
+++ book/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'book'
 urlpatterns = [
-    path('', views.home_page),
+    path('', views.home_page, name='home'),
     path('category/', views.category_view, name='category'),
     path('category-edit/<int:id>', views.category_edit_view, name='category edit'),
     path('category-register/', views.category_register_view, name='category register'),
