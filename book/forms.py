0fc2268e0ee8d3ea7d827b381b48c7154607ef1b
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-26 19:17:38 +0900

    add fields on forms.py

--- book/forms.py
+++ book/forms.py
@@ -5,8 +5,10 @@ from .models import Author, Category
 class AuthorForm(forms.ModelForm):
     class Meta:
         model = Author
+        fields = '__all__'
 
 
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
+        fields = '__all__'
