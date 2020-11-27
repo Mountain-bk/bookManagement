a3ac381f489c42ef5139f0805280bd132e605db9
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-11-27 23:39:28 +0900

    add BookForm on forms.py

--- book/forms.py
+++ book/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Author, Category
+from .models import Author, Category, Book
+from django.forms.widgets import CheckboxSelectMultiple
 
 
 class AuthorForm(forms.ModelForm):
@@ -12,3 +13,27 @@ class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = '__all__'
+
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = [
+            'title',
+            'published_date',
+            'categories',
+            'authors'
+        ]
+
+        widgets = {
+            'published_date': forms.DateInput(attrs={"type": "date"}),
+            'categories': CheckboxSelectMultiple(),
+            'authors': CheckboxSelectMultiple()
+        }
+
+        labels = {
+            'title': 'Title',
+            'published_date': 'Published Date',
+            'categories': 'Category',
+            'authors': 'Author'
+        }
