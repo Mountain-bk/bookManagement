6727adb911a2f37209ce81c4f2aa633001d23a55
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-12-07 14:21:16 +0900

    add book register test method on tests.py

--- book/tests.py
+++ book/tests.py
@@ -18,3 +18,23 @@ def setup():
     b1.save()
     b1.categories.add(c1)
     b1.authors.add(a1)
+
+
+@pytest.mark.django_db
+def test_duplicate_book_register_form():
+    c1 = Category.objects.get(name='Novel')
+    a1 = Author.objects.get(name='サン・テグジュペリ')
+    b2 = BookForm(
+        {
+            'title': '星の王子様',
+            'published_date': '2006-03-28',
+            'categories': [c1],
+            'authors': [a1]
+        }
+    )
+    if b2.is_valid():
+        b2.save()
+    else:
+        pass
+    # count will be 1 if it's duplicate register
+    assert Book.objects.count() == 1
