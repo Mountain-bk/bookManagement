b3928d61f320813bce5dde1ed71d0facff6833c6
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-12-07 09:51:15 +0900

    add pytest fixture setup() on tests.py

--- book/tests.py
+++ book/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+import pytest
+from .models import Book, Author, Category
+from .forms import BookForm, AuthorForm, CategoryForm
 
 # Create your tests here.
+
+
+@pytest.fixture(autouse=True)
+@pytest.mark.django_db
+def setup():
+    c1 = Category(name='Novel')
+    c1.save()
+    a1 = Author(name='サン・テグジュペリ')
+    a1.save()
+    b1 = Book(title='星の王子様',
+              published_date='2006-03-28')
+    b1.save()
+    b1.categories.add(c1)
+    b1.authors.add(a1)
