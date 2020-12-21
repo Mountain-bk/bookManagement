bc519dbab704966db0f718d2e298395772d0cc55
Ryo Yamamoto <ryochaaaan0414@gmail.com>
2020-12-21 23:44:59 +0900

    modify test_book.py

--- book/tests/test_book.py
+++ book/tests/test_book.py
@@ -4,167 +4,116 @@ from book.models import Book, Author, Category
 from book.forms import BookForm, AuthorForm, CategoryForm
 
 
-# test1 register(title not duplicate)
-# test2 register(all duplicate)
-# test3 register(title duplicate, other not)
-# test4 register(title and author duplicate, other not)
+@pytest.fixture()
+def setup_book():
+    Category.objects.bulk_create([
+        Category(name='Novel'),
+        Category(name='Design')
+    ])
+    Author.objects.bulk_create([
+        Author(name='サン・テグジュペリ'),
+        Author(name='池澤夏樹')
+    ])
+    b1 = Book(title='星の王子様',
+              published_date='2006-03-28')
+    b1.save()
+    b2 = Book(title='人間の大地', published_date='2015-08-20')
+    b2.save()
+    c1 = Category.objects.get(name='Novel')
+    # c2 = Category.objects.get(name='Design')
+    a1 = Author.objects.get(name='サン・テグジュペリ')
+    a2 = Author.objects.get(name='池澤夏樹')
+    b1.categories.add(c1)
+    b1.authors.add(a1, a2)
+    b2.categories.add(c1)
+    b2.authors.add(a1)
 
 
-class TestCategoryFunction():
-    #
-    @pytest.fixture()
-    def setup_category(self):
-        return Category.objects.bulk_create([
-            Category(name='Novel'),
-            Category(name='Design')
-        ])
+def test_book_register_case1(setup_book, client):
+    c1_pk = Category.objects.get(name='Novel').pk
+    a1_pk = Author.objects.get(name='サン・テグジュペリ').pk
 
-    def test_category_register_case1(self, setup_category, client):
-        form_data = {'name': 'Programming'}
-        response = client.post('/category-register/', form_data, follow=True)
-        categories = list(response.context['categories'])
-        category_list = []
-        for category in categories:
-            category_list.append(category.name)
-        messages = list(response.context['messages'])
-        response_message = messages[0]
-        assert response_message.message == 'Form submission successful'
-        assert 'Programming' in category_list
-
-    def test_category_register_case2(self, setup_category, client):
-        form_data = {'name': 'Novel'}
-        response = client.post('/category-register/', form_data, follow=True)
-        categories = list(response.context['categories'])
-        category_list = []
-        for category in categories:
-            category_list.append(category.name)
-        messages = list(response.context['messages'])
-        response_message = messages[0]
-        assert response_message.message == 'Sorry, same category already exists'
-        assert category_list.count('Novel') == 1
-
-    def test_category_edit_case1(self, setup_category, client):
-        category = Category.objects.get(name='Novel')
-        response = client.post(reverse('book:category edit', kwargs={'id': category.id}), {
-                               'name': 'Python'}, follow=True)
-        categories = list(response.context['categories'])
-        category_list = []
-        for category in categories:
-            category_list.append(category.name)
-        messages = list(response.context['messages'])
-        response_message = messages[0]
-        assert response_message.message == 'Update submission succesfull'
-        assert 'Design', 'Python' in category_list == True
-
-    def test_category_edit_case2(self, setup_category, client):
-        category = Category.objects.get(name='Novel')
-        response = client.post(reverse('book:category edit', kwargs={'id': category.id}), {
-                               'name': 'Design'}, follow=True)
-        categories = list(response.context['categories'])
-        category_list = []
-        for category in categories:
-            category_list.append(category.name)
-        messages = list(response.context['messages'])
-        response_message = messages[0]
-        assert response_message.message == 'Sorry, same category already exists'
-        assert category_list.count('Novel') == 1
+    form_data = {
+        'title': '星の王子様',
+        'published_date': '2015-08-20',
+        'categories': [c1_pk],
+        'authors': [a1_pk]
+    }
+    response = client.post('/book-register/', form_data, follow=True)
+    books = list(response.context['books'])
+    book_list = []
+    for book in books:
+        book_list.append(book.title)
+    messages = list(response.context['messages'])
+    response_message = messages[0]
+    assert response_message.message == 'Form submission successful'
+    assert book_list.count('星の王子様') == 2
 
 
-class TestAuthorFunction():
-    @pytest.fixture()
-    def setup_author(self):
-        return Author.objects.bulk_create([
-            Author(name='サン・テグジュペリ'),
-            Author(name='池澤夏樹')
-        ])
+def test_book_register_case2(setup_book, client):
+    c1_pk = Category.objects.get(name='Novel').pk
+    c2_pk = Category.objects.get(name='Design').pk
+    a1_pk = Author.objects.get(name='サン・テグジュペリ').pk
+    a2_pk = Author.objects.get(name='池澤夏樹').pk
 
-    def test_author_register_case1(self, setup_author, client):
-        form_data = {'name': '村上春樹'}
-        response = client.post('/author-register/', form_data, follow=True)
-        authors = list(response.context['authors'])
-        author_list = []
-        for author in authors:
-            author_list.append(author.name)
-        messages = list(response.context['messages'])
-        response_message = messages[0]
-        assert response_message.message == 'Form submission successful'
-        assert '村上春樹' in author_list
-
-    def test_author_register_case2(self, setup_author, client):
-        form_data = {'name': 'サン・テグジュペリ'}
-        response = client.post('/author-register/', form_data, follow=True)
-        authors = list(response.context['authors'])
-        author_list = []
-        for author in authors:
-            author_list.append(author.name)
-        messages = list(response.context['messages'])
-        response_message = messages[0]
-        assert response_message.message == 'Sorry, same author already exists'
-        assert author_list.count('サン・テグジュペリ') == 1
-
-    def test_author_edit_case1(self, setup_author, client):
-        a1 = Author.objects.get(name='サン・テグジュペリ')
-        response = client.post(reverse('book:author edit', kwargs={'id': a1.id}), {
-                               'name': '東野圭吾'}, follow=True)
-        authors = list(response.context['authors'])
-        author_list = []
-        for author in authors:
-            author_list.append(author.name)
-        messages = list(response.context['messages'])
-        response_message = messages[0]
-        assert response_message.message == 'Update submission succesfull'
-        assert '池澤夏樹', '東野圭吾' in author_list == True
-
-    def test_author_edit_case2(self, setup_author, client):
-        a1 = Author.objects.get(name='サン・テグジュペリ')
-        response = client.post(reverse('book:author edit', kwargs={'id': a1.id}), {
-                               'name': '池澤夏樹'}, follow=True)
-        authors = list(response.context['authors'])
-        author_list = []
-        for author in authors:
-            author_list.append(author.name)
-        messages = list(response.context['messages'])
-        response_message = messages[0]
-        assert response_message.message == 'Sorry, same author already exists'
-        assert author_list.count('池澤夏樹') == 1
+    form_data = {
+        'title': '星の王子様',
+        'published_date': '2015-08-20',
+        'categories': [c1_pk, c2_pk],
+        'authors': [a1_pk, a2_pk]
+    }
+    response = client.post('/book-register/', form_data, follow=True)
+    books = list(response.context['books'])
+    book_list = []
+    for book in books:
+        book_list.append(book.title)
+    messages = list(response.context['messages'])
+    response_message = messages[0]
+    assert response_message.message == 'Sorry, there is an error.'
+    assert book_list.count('星の王子様') == 1
 
 
-class TestBookFunction():
-    @pytest.fixture()
-    def setup_book(self):
-        Category.objects.bulk_create([
-            Category(name='Novel'),
-            Category(name='Design')
-        ])
-        Author.objects.bulk_create([
-            Author(name='サン・テグジュペリ'),
-            Author(name='池澤夏樹')
-        ])
-        b1 = Book(title='星の王子様',
-                  published_date='2006-03-28')
-        b1.save()
-        c1 = Category.objects.get(name='Novel')
-        # c2 = Category.objects.get(name='Design')
-        a1 = Author.objects.get(name='サン・テグジュペリ')
-        # a2 = Author.objects.get(name='池澤 夏樹')
-        b1.categories.add(c1)
-        b1.authors.add(a1)
-        return b1
+def test_book_edit_case1(setup_book, client):
+    b1_pk = Book.objects.get(title='人間の大地').pk
+    c1_pk = Category.objects.get(name='Novel').pk
+    a1_pk = Author.objects.get(name='サン・テグジュペリ').pk
+    form_data = {
+        'title': '星の王子様',
+        'published_date': '2015-08-20',
+        'categories': [c1_pk],
+        'authors': [a1_pk]
+    }
+    response = client.post(reverse('book:book edit', kwargs={
+                           'id': b1_pk}), form_data, follow=True)
 
-    def test_demo(self, setup_book, client):
-        r = client.get('/book-register/')
-        con = r.context
-        widget = r.context['widget']
-        print(con)
-        print(widget)
+    books = list(response.context['books'])
+    book_list = []
+    for book in books:
+        book_list.append(book.title)
+    messages = list(response.context['messages'])
+    response_message = messages[0]
+    assert response_message.message == 'Update submission succesfull'
+    assert book_list.count('星の王子様') == 2
 
-        form_data = {
-            'title': '星の王子様',
-            'published_date': '1995-12-28',
-            'categories': [1],
-            'authors': [1]
-        }
-        response = client.post('/book-register/', form_data)
-        print(response)
-        # assert form_data.is_valid() == True
-        assert response.url == '/book-shelf/'
+
+def test_book_edit_case2(setup_book, client):
+    b1_pk = Book.objects.get(title='人間の大地').pk
+    a1_pk = Author.objects.get(name='サン・テグジュペリ').pk
+    a2_pk = Author.objects.get(name='池澤夏樹').pk
+    c1_pk = Category.objects.get(name='Novel')
+    form_data = {
+        'title': '星の王子様',
+        'published_date': '2015-08-20',
+        'categories': [c1_pk],
+        'authors': [a1_pk, a2_pk]
+    }
+    response = client.post(reverse('book:book edit', kwargs={
+                           'id': b1_pk}), form_data, follow=True)
+    books = list(response.context['books'])
+    book_list = []
+    for book in books:
+        book_list.append(book.title)
+    messages = list(response.context['messages'])
+    response_message = messages[0]
+    assert response_message.message == 'Sorry, there is an error.'
+    assert book_list.count('星の王子様') == 1
