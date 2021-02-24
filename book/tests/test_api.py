d05510a20128851caab3c179e489d46f34e403ed
Ryo <ryochaaaan0414@gmail.com>
2021-02-24 20:10:37 +0900

    new django rest framework api test code

--- book/tests/test_api.py
+++ book/tests/test_api.py
@@ -32,7 +32,7 @@ def test_get_book_list_detail_api(setup_book_objects, client):
     c2_pk = Category.objects.get(name="Category2").pk
     a1_pk = Author.objects.get(name="Author1").pk
     a2_pk = Author.objects.get(name="Author2").pk
-    get_response = client.get("/api/books")
+    get_response = client.get("/books/")
     get_response_status = get_response.status_code
     assert get_response_status == 200
     get_response_data = get_response.content.decode("utf-8")
@@ -55,7 +55,8 @@ def test_get_book_list_detail_api(setup_book_objects, client):
                 "categories": [{"id": c2_pk, "name": "Category2"}],
                 "authors": [{"id": a1_pk, "name": "Author1"}],
             },
-        ]
+        ],
+        separators=(",", ":"),
     )
     assert expected_data == get_response_data
 
@@ -76,7 +77,7 @@ def test_post_book_detail_api(setup_book_objects, client):
         "authors": [{"id": a1_pk, "name": "Author1"}, {"id": a2_pk, "name": "Author2"}],
     }
     post_response_status = client.post(
-        "/api/books", data=json.dumps(form_data), content_type="application/json"
+        "/books/", data=json.dumps(form_data), content_type="application/json"
     ).status_code
     assert post_response_status == 201
     b3_pk = Book.objects.get(title="Book3").pk
@@ -109,9 +110,10 @@ def test_post_book_detail_api(setup_book_objects, client):
                     {"id": a2_pk, "name": "Author2"},
                 ],
             },
-        ]
+        ],
+        separators=(",", ":"),
     )
-    get_response_data = client.get("/api/books").content.decode("utf-8")
+    get_response_data = client.get("/books/").content.decode("utf-8")
     assert expected_data == get_response_data
 
 
@@ -121,7 +123,7 @@ def test_get_book_detail_api(setup_book_objects, client):
     c1_pk = Category.objects.get(name="Category1").pk
     a1_pk = Author.objects.get(name="Author1").pk
     a2_pk = Author.objects.get(name="Author2").pk
-    get_response_data = client.get("/api/books/" + str(b1_pk)).content.decode("utf-8")
+    get_response_data = client.get("/books/" + str(b1_pk) + "/").content.decode("utf-8")
     expected_data = json.dumps(
         {
             "id": b1_pk,
@@ -132,7 +134,8 @@ def test_get_book_detail_api(setup_book_objects, client):
                 {"id": a1_pk, "name": "Author1"},
                 {"id": a2_pk, "name": "Author2"},
             ],
-        }
+        },
+        separators=(",", ":"),
     )
     assert expected_data == get_response_data
 
@@ -150,7 +153,7 @@ def test_put_book_detail_api(setup_book_objects, client):
         "authors": [{"id": a1_pk, "name": "Author1"}],
     }
     put_response_status = client.put(
-        "/api/books/" + str(b1_pk),
+        "/books/" + str(b1_pk) + "/",
         data=json.dumps(form_data),
         content_type="application/json",
     ).status_code
@@ -162,9 +165,10 @@ def test_put_book_detail_api(setup_book_objects, client):
             "published_date": "1995-03-28",
             "categories": [{"id": c1_pk, "name": "Category1"}],
             "authors": [{"id": a1_pk, "name": "Author1"}],
-        }
+        },
+        separators=(",", ":"),
     )
-    get_response_data = client.get("/api/books/" + str(b1_pk)).content.decode("utf-8")
+    get_response_data = client.get("/books/" + str(b1_pk) + "/").content.decode("utf-8")
     assert expected_data == get_response_data
 
 
@@ -174,9 +178,9 @@ def test_delete_book_api(setup_book_objects, client):
     b2_pk = Book.objects.get(title="Book2").pk
     c2_pk = Category.objects.get(name="Category2").pk
     a1_pk = Author.objects.get(name="Author1").pk
-    delete_response_status = client.delete("/api/books/" + str(b1_pk)).status_code
+    delete_response_status = client.delete("/books/" + str(b1_pk) + "/").status_code
     assert delete_response_status == 204
-    get_response_data = client.get("/api/books").content.decode("utf-8")
+    get_response_data = client.get("/books/").content.decode("utf-8")
     expected_data = json.dumps(
         [
             {
@@ -186,7 +190,8 @@ def test_delete_book_api(setup_book_objects, client):
                 "categories": [{"id": c2_pk, "name": "Category2"}],
                 "authors": [{"id": a1_pk, "name": "Author1"}],
             }
-        ]
+        ],
+        separators=(",", ":"),
     )
     assert expected_data == get_response_data
 
@@ -195,7 +200,7 @@ def test_delete_book_api(setup_book_objects, client):
 def test_get_author_list_detail_api(setup_book_objects, client):
     a1_pk = Author.objects.get(name="Author1").pk
     a2_pk = Author.objects.get(name="Author2").pk
-    get_response = client.get("/api/authors")
+    get_response = client.get("/authors/")
     get_response_status = get_response.status_code
     assert get_response_status == 200
     response_data = get_response.content.decode("utf-8")
@@ -203,7 +208,8 @@ def test_get_author_list_detail_api(setup_book_objects, client):
         [
             {"id": a1_pk, "name": "Author1"},
             {"id": a2_pk, "name": "Author2"},
-        ]
+        ],
+        separators=(",", ":"),
     )
     assert expected_data == response_data
 
@@ -214,7 +220,7 @@ def test_post_author_detail_api(setup_book_objects, client):
     a2_pk = Author.objects.get(name="Author2").pk
     form_data = {"id": None, "name": "Author3"}
     post_response_status = client.post(
-        "/api/authors", data=json.dumps(form_data), content_type="application/json"
+        "/authors/", data=json.dumps(form_data), content_type="application/json"
     ).status_code
     assert post_response_status == 201
     a3_pk = Author.objects.get(name="Author3").pk
@@ -223,18 +229,24 @@ def test_post_author_detail_api(setup_book_objects, client):
             {"id": a1_pk, "name": "Author1"},
             {"id": a2_pk, "name": "Author2"},
             {"id": a3_pk, "name": "Author3"},
-        ]
+        ],
+        separators=(",", ":"),
     )
-    get_response_data = client.get("/api/authors").content.decode("utf-8")
+    get_response_data = client.get("/authors/").content.decode("utf-8")
     assert expected_data == get_response_data
 
 
 # Test retrieving individual author detail
 def test_get_author_detail_api(setup_book_objects, client):
     a1_pk = Author.objects.get(name="Author1").pk
-    get_response_data = client.get("/api/authors/" + str(a1_pk)).content.decode("utf-8")
+    get_response_data = client.get("/authors/" + str(a1_pk) + "/").content.decode(
+        "utf-8"
+    )
     expected_data = {"id": a1_pk, "name": "Author1"}
-    expected_data = json.dumps(expected_data)
+    expected_data = json.dumps(
+        expected_data,
+        separators=(",", ":"),
+    )
     assert expected_data == get_response_data
 
 
@@ -246,17 +258,18 @@ def test_put_author_detail_api(setup_book_objects, client):
     a2_pk = Author.objects.get(name="Author2").pk
     form_data = {"id": a1_pk, "name": "Update Author"}
     put_response_status = client.put(
-        "/api/authors/" + str(a1_pk),
+        "/authors/" + str(a1_pk) + "/",
         data=json.dumps(form_data),
         content_type="application/json",
     ).status_code
     assert put_response_status == 200
-    authors_get_response_data = client.get("/api/authors").content.decode("utf-8")
+    authors_get_response_data = client.get("/authors/").content.decode("utf-8")
     authors_expected_data = json.dumps(
         [
             {"id": a2_pk, "name": "Author2"},
             {"id": a1_pk, "name": "Update Author"},
-        ]
+        ],
+        separators=(",", ":"),
     )
     assert authors_expected_data == authors_get_response_data
     books_expected_data = json.dumps(
@@ -269,9 +282,10 @@ def test_put_author_detail_api(setup_book_objects, client):
                 {"id": a2_pk, "name": "Author2"},
                 {"id": a1_pk, "name": "Update Author"},
             ],
-        }
+        },
+        separators=(",", ":"),
     )
-    books_get_response_data = client.get("/api/books/" + str(b1_pk)).content.decode(
+    books_get_response_data = client.get("/books/" + str(b1_pk) + "/").content.decode(
         "utf-8"
     )
     assert books_expected_data == books_get_response_data
@@ -285,12 +299,15 @@ def test_delete_author_api(setup_book_objects, client):
     c2_pk = Category.objects.get(name="Category2").pk
     a1_pk = Author.objects.get(name="Author1").pk
     a2_pk = Author.objects.get(name="Author2").pk
-    delete_response_status = client.delete("/api/authors/" + str(a1_pk)).status_code
+    delete_response_status = client.delete("/authors/" + str(a1_pk) + "/").status_code
     assert delete_response_status == 204
-    authors_get_response_data = client.get("/api/authors").content.decode("utf-8")
-    authors_expected_data = json.dumps([{"id": a2_pk, "name": "Author2"}])
+    authors_get_response_data = client.get("/authors/").content.decode("utf-8")
+    authors_expected_data = json.dumps(
+        [{"id": a2_pk, "name": "Author2"}],
+        separators=(",", ":"),
+    )
     assert authors_expected_data == authors_get_response_data
-    books_get_response_data = client.get("/api/books").content.decode("utf-8")
+    books_get_response_data = client.get("/books/").content.decode("utf-8")
     books_expected_data = json.dumps(
         [
             {
@@ -309,7 +326,8 @@ def test_delete_author_api(setup_book_objects, client):
                 "categories": [{"id": c2_pk, "name": "Category2"}],
                 "authors": [],
             },
-        ]
+        ],
+        separators=(",", ":"),
     )
     assert books_expected_data == books_get_response_data
 
@@ -318,7 +336,7 @@ def test_delete_author_api(setup_book_objects, client):
 def test_get_category_list_detail_api(setup_book_objects, client):
     c1_pk = Category.objects.get(name="Category1").pk
     c2_pk = Category.objects.get(name="Category2").pk
-    get_response = client.get("/api/categories")
+    get_response = client.get("/categories/")
     get_response_status = get_response.status_code
     assert get_response_status == 200
     response_data = get_response.content.decode("utf-8")
@@ -326,7 +344,8 @@ def test_get_category_list_detail_api(setup_book_objects, client):
         [
             {"id": c1_pk, "name": "Category1"},
             {"id": c2_pk, "name": "Category2"},
-        ]
+        ],
+        separators=(",", ":"),
     )
     assert expected_data == response_data
 
@@ -337,7 +356,7 @@ def test_post_category_detail_api(setup_book_objects, client):
     c2_pk = Category.objects.get(name="Category2").pk
     form_data = {"id": None, "name": "Category3"}
     post_response_status = client.post(
-        "/api/categories", data=json.dumps(form_data), content_type="application/json"
+        "/categories/", data=json.dumps(form_data), content_type="application/json"
     ).status_code
     assert post_response_status == 201
     c3_pk = Category.objects.get(name="Category3").pk
@@ -346,20 +365,23 @@ def test_post_category_detail_api(setup_book_objects, client):
             {"id": c1_pk, "name": "Category1"},
             {"id": c2_pk, "name": "Category2"},
             {"id": c3_pk, "name": "Category3"},
-        ]
+        ],
+        separators=(",", ":"),
     )
-    get_response_data = client.get("/api/categories").content.decode("utf-8")
+    get_response_data = client.get("/categories/").content.decode("utf-8")
     assert expected_data == get_response_data
 
 
 # Test retrieving individual category detail
 def test_get_category_detail_api(setup_book_objects, client):
     c1_pk = Category.objects.get(name="Category1").pk
-    get_response_data = client.get("/api/categories/" + str(c1_pk)).content.decode(
+    get_response_data = client.get("/categories/" + str(c1_pk) + "/").content.decode(
         "utf-8"
     )
-    expected_data = {"id": c1_pk, "name": "Category1"}
-    expected_data = json.dumps(expected_data)
+    expected_data = json.dumps(
+        {"id": c1_pk, "name": "Category1"},
+        separators=(",", ":"),
+    )
     assert expected_data == get_response_data
 
 
@@ -372,17 +394,18 @@ def test_put_category_detail_api(setup_book_objects, client):
     a2_pk = Author.objects.get(name="Author2").pk
     form_data = {"id": c1_pk, "name": "Update Category"}
     put_response_status = client.put(
-        "/api/categories/" + str(c1_pk),
+        "/categories/" + str(c1_pk) + "/",
         data=json.dumps(form_data),
         content_type="application/json",
     ).status_code
     assert put_response_status == 200
-    categories_get_response_data = client.get("/api/categories").content.decode("utf-8")
+    categories_get_response_data = client.get("/categories/").content.decode("utf-8")
     categories_expected_data = json.dumps(
         [
             {"id": c2_pk, "name": "Category2"},
             {"id": c1_pk, "name": "Update Category"},
-        ]
+        ],
+        separators=(",", ":"),
     )
     assert categories_expected_data == categories_get_response_data
     books_expected_data = json.dumps(
@@ -395,9 +418,10 @@ def test_put_category_detail_api(setup_book_objects, client):
                 {"id": a1_pk, "name": "Author1"},
                 {"id": a2_pk, "name": "Author2"},
             ],
-        }
+        },
+        separators=(",", ":"),
     )
-    books_get_response_data = client.get("/api/books/" + str(b1_pk)).content.decode(
+    books_get_response_data = client.get("/books/" + str(b1_pk) + "/").content.decode(
         "utf-8"
     )
     assert books_expected_data == books_get_response_data
@@ -411,12 +435,17 @@ def test_delete_category_api(setup_book_objects, client):
     c2_pk = Category.objects.get(name="Category2").pk
     a1_pk = Author.objects.get(name="Author1").pk
     a2_pk = Author.objects.get(name="Author2").pk
-    delete_response_status = client.delete("/api/categories/" + str(c1_pk)).status_code
+    delete_response_status = client.delete(
+        "/categories/" + str(c1_pk) + "/"
+    ).status_code
     assert delete_response_status == 204
-    categories_get_response_data = client.get("/api/categories").content.decode("utf-8")
-    categories_expected_data = json.dumps([{"id": c2_pk, "name": "Category2"}])
+    categories_get_response_data = client.get("/categories/").content.decode("utf-8")
+    categories_expected_data = json.dumps(
+        [{"id": c2_pk, "name": "Category2"}],
+        separators=(",", ":"),
+    )
     assert categories_expected_data == categories_get_response_data
-    books_get_response_data = client.get("/api/books").content.decode("utf-8")
+    books_get_response_data = client.get("/books/").content.decode("utf-8")
     books_expected_data = json.dumps(
         [
             {
@@ -436,6 +465,7 @@ def test_delete_category_api(setup_book_objects, client):
                 "categories": [{"id": c2_pk, "name": "Category2"}],
                 "authors": [{"id": a1_pk, "name": "Author1"}],
             },
-        ]
+        ],
+        separators=(",", ":"),
     )
     assert books_expected_data == books_get_response_data
